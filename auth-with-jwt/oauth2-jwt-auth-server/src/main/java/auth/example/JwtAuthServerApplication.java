14dab95bcc76006d69fc4a8c81f83d255c1a1df2
Qing Zhou <qzhou@thoughtworks.com>
2017-10-12 21:04:16 +0800

    create jwt auth server  application

---
+++ auth-with-jwt/oauth2-jwt-auth-server/src/main/java/auth/example/JwtAuthServerApplication.java
@@ -0,0 +1,117 @@
+package auth.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+
+@SpringBootApplication
+public class JwtAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthServerApplication.class, args);
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Configuration extends AuthorizationServerConfigurerAdapter {
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+            super.configure(security);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("web_app")
+                    .scopes("FOO")
+                    .authorities("FOO_READ")
+                    .authorizedGrantTypes("implicit", "refresh_token", "password");
+        }
+
+        @Autowired
+        @Qualifier("authenticationManagerBean")
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.tokenStore(tokenStore()).tokenEnhancer(jwtTokenEnhancer()).authenticationManager(authenticationManager);
+        }
+
+        @Bean
+        public TokenStore tokenStore() {
+            return new JwtTokenStore(jwtTokenEnhancer());
+        }
+
+        @Bean
+        protected JwtAccessTokenConverter jwtTokenEnhancer() {
+            KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"), "mySecretKey".toCharArray());
+            JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+            converter.setKeyPair(keyStoreKeyFactory.getKeyPair("jwt"));
+            return converter;
+        }
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    protected static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        @Bean
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManagerBean();
+        }
+
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http
+//                    .csrf().disable()
+//                    .exceptionHandling()
+//                    .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+//                    .and()
+//                    .authorizeRequests()
+//                    .antMatchers("/**").authenticated()
+//                    .and()
+//                    .httpBasic();
+//        }
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("reader")
+                    .password("reader")
+                    .authorities("FOO_READ", "ACTUATOR");
+        }
+    }
+
+    @RestController
+    protected static class HelloController {
+        @RequestMapping("/hello")
+        private String hello() {
+            return "hello";
+        }
+    }
+
+}
